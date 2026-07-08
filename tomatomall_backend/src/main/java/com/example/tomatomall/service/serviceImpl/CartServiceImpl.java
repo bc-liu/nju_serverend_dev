40ea12364315afb083b7a4918a3f6adb660e372c
bc-liu <2217390389@qq.com>
2026-07-08 23:26:07 +0800

    换用了数据库行锁方案，能扣完库存，修复了bug，但qps从2128降低到1063

--- tomatomall_backend/src/main/java/com/example/tomatomall/service/serviceImpl/CartServiceImpl.java
+++ tomatomall_backend/src/main/java/com/example/tomatomall/service/serviceImpl/CartServiceImpl.java
@@ -42,6 +42,7 @@ public class CartServiceImpl implements CartService {
     @Autowired
     private CartsOrdersRelationRepository cartsOrdersRelationRepository;
 
+    
     @Autowired(required = false)
     private KafkaTemplate<String, String> kafkaTemplate;
 
